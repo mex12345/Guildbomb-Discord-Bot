391946f567a53f07fe9836f7d41b6f06ee41561c
mex12345 <90515684+mex12345@users.noreply.github.com>
2021-09-11 14:19:32 +0000

    Update data.py

--- Guildbomb/UtilsDirectory/data.py
+++ Guildbomb/UtilsDirectory/data.py
@@ -5,8 +5,8 @@ import os
 
 ########################################################################################################################
 # Paste your token and a custom prefix right here:
-dc_token = "X"  # DISCORD-TOKEN
-dc_prefix = "!!"  # BOT-PREFIX
+dc_token = "ODg2MjUxODU3NjYyNTg2OTIx.YTy4XA.lY4HLQ70jFkzv2EtbnWHzGTZcGM"  # DISCORD-TOKEN
+dc_prefix = "mex!"  # BOT-PREFIX
 
 ########################################################################################################################
 
